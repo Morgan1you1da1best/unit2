5e524143fc858c89cbc1930177a4285857b16bc5
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-13 13:57:55 -0400

    Updated from Brython Server: 9/13/2017 1:57:54 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,11 @@
+#Morgan Baughman
+#9/13/17
+#movie.py - Can you watch this movie?
 
+age= int(input('Enter age: '))
+if age>=13:
+    print('You can watch PG-13 movies')
+if age>=17:
+    print('You can watch R movies')
+if age>=21:
+    print('You can watch MA movies')

df26621d94e116757ad9b5d7f1eafe4daeb00bcf
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-14 14:37:14 -0400

    Updated from Brython Server: 9/14/2017 2:37:14 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,24 @@
+#Morgan Baughman
+#9/14/17
+#fortuneTeller.py
 
+color = input('Enter a color (red or blue): ')
+red = 'red'
+blue = 'blue'
+number = int(input('Enter a number between 1-4: '))
+if color == red and number == 1:
+    print('You will die by gunshot')
+if color == red and number == 2:
+    print('You will die by asphyxiation')
+if color == red and number == 3:
+    print('You have been awarded a lifetime supply of chicken nuggets')
+if color == red and number == 4:
+    print('You will achieve great things')
+if color == blue and number == 1:
+    print('You will win the lottery')
+if color == blue and number == 2:
+    print('You will die by disease')
+if color == blue and number == 3:
+    print('You will die of heat exhaustion')
+if color == blue and number == 4:
+    print('You will die by blood loss')

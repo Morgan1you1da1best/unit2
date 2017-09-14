e9e2c959150f423f054f00d0dc27a094a572596d
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-14 14:51:38 -0400

    Updated from Brython Server: 9/14/2017 2:51:37 PM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,10 @@
+#Morgan Baughman
+#9/14/17
+#coinFlip - Flipping a coin
 
+from random import randint
+coinflip = randint(1,2)
+if coinflip == 1:
+    print('heads')
+if coinflip == 2:
+    print('tails') 

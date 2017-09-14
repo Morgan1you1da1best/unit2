e3cb096594065ccd845a11b95b5e57124e326090
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-14 14:23:32 -0400

    Updated from Brython Server: 9/14/2017 2:23:32 PM

--- compoundConditionalDemo.py
+++ compoundConditionalDemo.py
@@ -4,4 +4,12 @@
 
 num = float(input('Enter a number: '))
 if num>0 and num%7 == 0:
-    print('Your number is postive and divisable by 7')
+    print('Your number is postive and divisable by 7.')
+elif num>0:
+    print('Your number is positive and not divisable by 7.')
+elif num<0 and num%7 == 0:
+    print('Your number is negative and divisable by 7.')
+elif num<0:
+    print('Your number is negative and not divisable by 7.')
+else:
+        print('Your number is 0.')

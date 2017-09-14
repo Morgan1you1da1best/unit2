bb762aeb95ecccaf284f8c3bf4631b40aa7ffe09
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-14 14:47:24 -0400

    Updated from Brython Server: 9/14/2017 2:47:24 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,16 @@
+#Morgan Baughman
+#9/14/17
+#insulter.py - Insult your freinds
 
+from random import randint
+randint(1,5)
+name = input('Enter your name here: ')
+number = randint(1,5)
+if number == 1:
+    print('Your feet stink')
+if number == 2:
+    print('What is wrong with your face?')
+if number == 3:
+    print('You are lame')
+if number == 4:
+    print('Did you get a haircut? Your hair looks different today')

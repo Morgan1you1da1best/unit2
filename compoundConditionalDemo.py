434284fb2b8bf748778e1ee9d8a1a552ee9ab016
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-14 14:18:26 -0400

    Updated from Brython Server: 9/14/2017 2:18:26 PM

--- compoundConditionalDemo.py
+++ compoundConditionalDemo.py
@@ -1,1 +1,7 @@
+#Morgan Baughman
+#9/14/17
+#compoundConditionalDemo.py - and/or statements
 
+num = float(input('Enter a number: '))
+if num>0 and num%7 == 0:
+    print('Your number is postive and divisable by 7')

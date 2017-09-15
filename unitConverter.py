555f8981febc458095b4840bf8a4c7cf9f1a55db
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-15 14:55:05 -0400

    Updated from Brython Server: 9/15/2017 2:55:04 PM

--- unitConverter.py
+++ unitConverter.py
@@ -2,20 +2,16 @@
 #9/15/17
 #unitConverter.py - COnverting Units of stuff
 
-one = ('1) Kilotmeters to Miles')
-two = ('2) Kilograms to Pounds')
-three = ('3) Liters to Gallons.')
-four = ('4) Celsius to Fahrenheit')
-
-four = 4
-three = 3
-two = 2
-one = 1
+a = ('1) Kilotmeters to Miles')
+b = ('2) Kilograms to Pounds')
+c = ('3) Liters to Gallons.')
+d = ('4) Celsius to Fahrenheit')
 
 print('choose a convertion.')
-print('1) Kilotmeters to Miles')
-print('2) Kilograms to Pounds')
-print('3) Liters to Gallons.')
-print('4) Celsius to Fahrenheit')
-num = int(input('choose a convertion: '))
+print('a) Kilotmeters to Miles')
+print('b) Kilograms to Pounds')
+print('c) Liters to Gallons.')
+print('d) Celsius to Fahrenheit')
+convertionopt = (input('choose a convertion: '))
+convertion = float(input('Enter' , a)
 

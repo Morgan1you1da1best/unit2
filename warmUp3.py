d55b5fa5048bed596cab2f3a2e1d72f9af2e6e75
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-15 14:30:50 -0400

    Updated from Brython Server: 9/15/2017 2:30:49 PM

--- warmUp3.py
+++ warmUp3.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#9/15/17
+#warmUp3.py - practicing if statements
 
+num = int(input('Enter a number: '))
+if num% 2 == 0 and num% 3 == 0:
+    print(num, 'is divisible by both 2 and 3')
+elif num% 2 == 0:
+    print(num, 'is divisible by 2')
+elif num% 3 == 0:
+    print(num, 'is divisible by 3')
+else:
+    print(num , 'is divisble by neither 2 or 3')

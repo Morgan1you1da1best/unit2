c887caf1e7f9a195a9ab8005a5ab0d1d9beed857
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-18 14:36:33 -0400

    Updated from Brython Server: 9/18/2017 2:36:32 PM

--- redTriangle.py
+++ redTriangle.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#8/9/17
+#redSquare.py 
 
+from ggame import*
+
+red = Color(0xff0000, 1)
+line = LineStyle(3,red)
+rectangle = RectangleAsset(100,100,line,red)
+
+Sprite(rectangle)
+myApp = App()
+myApp.run()

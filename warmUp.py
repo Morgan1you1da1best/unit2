a118c8d2afadd6c3b124563dbaa841a87e93af7a
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-18 14:25:58 -0400

    Updated from Brython Server: 9/18/2017 2:25:57 PM

--- warmUp.py
+++ warmUp.py
@@ -1,1 +1,9 @@
+#Morgan Baughman
+#9/18/27
+#warpUp4.py - Buzz game with 7s and things divisible by 7
 
+num = int(input('Enter a number: '))
+if '7' in str(num):
+    print('Buzz')
+elif num% 7==0:
+    print('Buzz')

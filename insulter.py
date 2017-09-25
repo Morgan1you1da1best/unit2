45bdfe9abea1b76341e2ae5a5ae16957dc7337e1
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-25 14:07:10 -0400

    Updated from Brython Server: 9/25/2017 2:07:09 PM

--- insulter.py
+++ insulter.py
@@ -3,14 +3,14 @@
 #insulter.py - Insult your freinds
 
 from random import randint
-randint(1,5)
+randint(1,4)
 name = input('Enter your name here: ')
-number = randint(1,5)
+number = randint(1,4)
 if number == 1:
-    print('Your feet stink')
+    print(name,', your feet stink')
 if number == 2:
-    print('What is wrong with your face?')
+    print(name,', what is wrong with your face?')
 if number == 3:
-    print('You are lame')
+    print(name,', you are lame.')
 if number == 4:
-    print('Did you get a haircut? Your hair looks different today')
+    print( name,', did you get a haircut? Your hair looks different today.')

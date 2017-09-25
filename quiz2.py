e252ef5608a5781d42246ff7a9569c04aee78a76
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-25 14:50:12 -0400

    Updated from Brython Server: 9/25/2017 2:50:11 PM

--- quiz2.py
+++ quiz2.py
@@ -6,16 +6,16 @@ num1 = float(input('Enter a number here: '))
 num2 = float(input('Enter another number here: '))
 
 if num1 == num2:
-    print('These numbers are the same')
+    print('These numbers are the same.')
 elif num1>num2:
     print(num1, 'is larger than', num2)
 elif num2>num1:
     print(num2, 'is larger than', num1)
 
 if num1% 3==0:
-    print(num1, 'is divisible by three')
+    print(num1, 'is divisible by three.')
 if num2% 3==0:
-    print(num2, 'is divisivle by three')
+    print(num2, 'is divisivle by three.')
 else: 
     print('neither of these numbers are divible by 3')
 answer = float(input('Product of your two numbers: '))
